fe9d809e466d2f9b0e4e750d7546d6eb815ce0dd
ssiddiqa <siddiqasayma@gmail.com>
2024-08-13 22:51:22 +0600

    done

--- js/main.js
+++ js/main.js
@@ -10,86 +10,98 @@ document.getElementById('change').addEventListener('mouseout', function () {
 function fetchRes() {
     const name = 'Triangle'
     const base = document.getElementById('f-input')
-    const b = parseFloat(base.value)
     const height = document.getElementById('s-input')
-    const h = parseFloat(height.value)
-    base.value = ' '
-    height.value = ' '
-    const half = 0.5
-    showRes(half, b, h, name)
+    convert(name, base, height)
 }
 //for rhombus
 function fetchRom() {
     const name = 'Rhombus'
     const base = document.getElementById('rm-w')
-    const b = parseFloat(base.value)
     const height = document.getElementById('rm-l')
-    const h = parseFloat(height.value)
-    base.value = ' '
-    height.value = ' '
-    const half = 0.5
-    showRes(half, b, h, name)
+    convert(name, base, height)
 }
 //for pentagon
 function fetchPen() {
     const name = 'Pentagon'
     const base = document.getElementById('pen-w')
-    const b = parseFloat(base.value)
     const height = document.getElementById('pen-l')
-    const h = parseFloat(height.value)
-    base.value = ' '
-    height.value = ' '
-    const half = 0.5
-    showRes(half, b, h, name)
+    convert(name, base, height)
 }
 //for ellipse
 function fetchEll() {
-    const name = 'Pentagon'
+    const name = 'Ellipse'
     const base = document.getElementById('e-w')
-    const b = parseFloat(base.value)
     const height = document.getElementById('e-l')
-    const h = parseFloat(height.value)
-    base.value = ' '
-    height.value = ' '
-    const half = Math.PI
-    showRes(half, b, h, name)
-}
-function showRes(half, b, h, name) {
-    const send = document.getElementById('result')
-    const ans = half * b * h
-    const result = parseFloat(ans.toFixed(3))
-    const li = document.createElement('li')
-    li.innerText = 'The area of ' + name + ' is: ' + result
-    send.appendChild(li)
+    convert(name, base, height)
 }
 // for rectangle
 function fetchRec() {
     const name = 'Rectangle'
     const base = document.getElementById('r-w')
-    const b = parseFloat(base.value)
     const height = document.getElementById('r-h')
-    const h = parseFloat(height.value)
-    base.value = ' '
-    height.value = ' '
-    showResult(b, h, name)
+    convert(name, base, height)
 }
 // for parallelogram
 function fetchPar() {
     const name = 'Parallelogram'
     const base = document.getElementById('p-w')
-    const b = parseFloat(base.value)
     const height = document.getElementById('p-h')
+    convert(name, base, height)
+}
+//getting values common
+function convert(name, base, height) {
+    const b = parseFloat(base.value)
     const h = parseFloat(height.value)
     base.value = ' '
     height.value = ' '
-    showResult(b, h, name)
+    var half = 0.5
+    if (name == 'Ellipse')
+        half = Math.PI
+    if (name == 'Parallelogram' || name == 'Rectangle')
+        showRes(0, b, h, name)
+    else
+        showRes(half, b, h, name)
 }
-function showResult(b, h, name) {
+//showing result 
+function showRes(half, b, h, name) {
     const send = document.getElementById('result')
-    const ans = b * h
+    var ans = half * b * h
+    if (ans == 0)
+        ans = b * h
     const result = parseFloat(ans.toFixed(3))
-    const li = document.createElement('li')
-    li.innerText = 'The area of ' + name + ' is: ' + result
-    send.appendChild(li)
+    const span = document.createElement('span')
+    span.innerText = 'The area of ' + name + ' is: '
+    const span1 = document.createElement('span')
+    span1.innerText = result
+    const span2 = document.createElement('span')
+    span2.innerHTML = ' cm&sup2'
+    send.appendChild(span)
+    send.appendChild(span1)
+    send.appendChild(span2)
+    span1.setAttribute('id', 'later')
 }
 
+
+// cm to m
+function toMeter() {
+    const hudai = document.getElementById('later').innerText
+    const meter = parseFloat(hudai / 100)
+    const final = meter.toFixed(3)
+    const send = document.getElementById('result')
+    const br = document.createElement('br')
+    send.appendChild(br)
+    const span = document.createElement('span')
+    span.innerText = 'The area in meter is: '
+    const span1 = document.createElement('span')
+    span1.innerText = final
+    const span2 = document.createElement('span')
+    span2.innerHTML = ' m&sup2'
+    send.appendChild(span)
+    send.appendChild(span1)
+    send.appendChild(span2)
+    const br1 = document.createElement('br')
+    send.appendChild(br1)
+    const del = document.getElementById('later')
+    del.removeAttribute('id')
+
+}
